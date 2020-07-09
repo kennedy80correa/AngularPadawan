767543da12137b7e1e014864d98b432e055da3a4
Kennedy Corrêa <kennedy80correa@gmail.com>
2020-07-09 14:17:45 -0300

    Criação do Projeto

---
+++ AngularPadawan/src/app/input/input.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-input',
+    templateUrl: './input.component.html',
+    styles:[`
+        border-radius: 2%;
+        border: 2px solid black;
+    `]
+})
+export class InputComponent{
+
+}
