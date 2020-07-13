56627cd5fa0fee56bbed6fe8fc1c105eb1064070
gtorres17 <gtorres89@outlook.com.br>
2020-07-13 15:05:02 -0300

    test

--- AngularPadawan/src/app/home/first/first.component.ts
+++ AngularPadawan/src/app/home/first/first.component.ts
@@ -5,18 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './first.component.html',
   styleUrls: ['./first.component.css']
 })
-<<<<<<< HEAD
-
-=======
->>>>>>> 5936e773c4c4fe399d9fedce27ca4fa0506b2742
 export class FirstComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
-<<<<<<< HEAD
-=======
-
->>>>>>> 5936e773c4c4fe399d9fedce27ca4fa0506b2742
 }
