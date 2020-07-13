55100f3ae59069a3ccb226e63092dc2c0569231c
gtorres17 <gtorres89@outlook.com.br>
2020-07-13 08:44:55 -0300

    smt

--- AngularPadawan/src/app/register/register.component.ts
+++ AngularPadawan/src/app/register/register.component.ts
@@ -16,10 +16,23 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onRegister(){
+  Message(){
     this.message = "Thanks for register!";
   }
 
+  onRegisterName(event: any){
+    this.names.push((<HTMLInputElement>event.target).value);
+  }
+
+  onRegisterUser(event: any){
+    this.users.push((<HTMLInputElement>event.target).value);
+  }
+
+  onRegisterPassword(event: any){
+    this.passwords.push((<HTMLInputElement>event.target).value);
+  }
+
+  
   // onRegister(event: any, event: any, event: any){
   //   this.names.push((<HTMLInputElement>event.target).value);
   //   this.users.push((<HTMLInputElement>event.target).value);
