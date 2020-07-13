15bdb4439d6a393b92eb3402047be60f0bb23fd9
Levi Titonelli <juninho.levi1@hotmail.com>
2020-07-13 16:13:58 -0300

    correto

--- AngularPadawan/src/app/home/home.component.ts
+++ AngularPadawan/src/app/home/home.component.ts
@@ -5,10 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-<<<<<<< HEAD
 
-=======
->>>>>>> 5936e773c4c4fe399d9fedce27ca4fa0506b2742
 export class HomeComponent implements OnInit {
 
   constructor() { }
